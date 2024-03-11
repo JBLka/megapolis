31b30738ca7d98a4006d3bd42b05f8c4d649d799
JBLka <iliyapolyakov2006@gmail.com>
2024-03-11 18:50:34 +0300

    Задача 4

--- 4.py
+++ 4.py
@@ -8,7 +8,6 @@ with open('products.csv', encoding='utf-8') as file:
         i["promocode"] = product[:2].upper() + date[0] + product[:-3:-1].upper() + date[1][::-1]
         # Генерируем промокод
 
-
 with open("product_promo.csv", "w", encoding='utf-8', newline="") as file:
     writer = csv.DictWriter(file, fieldnames=['Category', 'product', 'Date', 'Price per unit', 'Count', 'promocode'],
                             delimiter=';')
