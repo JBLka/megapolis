f4af78f09595938a45bee6cdc3a7f1fe924a362c
JBLka <iliyapolyakov2006@gmail.com>
2024-03-11 18:49:33 +0300

    Задача 2

--- 3.py
+++ 3.py
@@ -1,17 +1,16 @@
 import csv
 
-
 with open('products.csv', encoding='utf-8') as file:
     reader = list(csv.DictReader(file, delimiter=';'))
     Category = input()
-    while Category != "молоко":
+    while Category != "молоко":  # Пока мы не ввели "молоко"
         Count = 999999999999
         for i in reader:
             if i["Category"] == Category and float(i["Count"]) < Count:
-                Count = float(i["Count"])
-                product = i["product"]
-        if Count != 999999999999:
+                Count = float(i["Count"])  # Если категория та и количество купленных товаров меньше сохраненного
+                product = i["product"]  # Записываем количество продаж и название продукта
+        if Count != 999999999999:  # Если переменная поменялась, то мы нашли товары из этой категории
             print(f"В категории: {Category} товар: {product} был куплен {Count} раз")
-        else:
+        else:  # Если не поменялась - не нашли
             print("Такой категории не существует в нашей БД")
         Category = input()
